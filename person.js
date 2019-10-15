86736ed2364d89a1e12ce15371f7e39fe88c14cc
William Blacknall <williamblacknall@gmail.com>
2019-10-15 13:15:07 -0400

    Added delayed greeting function

--- person.js
+++ person.js
@@ -12,7 +12,22 @@ class Person {
     greet(other) {
         console.log(this.createGreeting(other));
     }
+    lazyGreet(other){
+        setTimeout(function (){
+            console.log(`Yo ${other.name}! From ${this.name}. What a bizarre way to phrase a greeting.`);
+            console.log("Here's the timeout, you absolute animal.")
+        }, 2000);
+    };
 }
 
+let sammy = new Person(
+    "Sammy",
+    ["David","Karla","Sam","Judith"]
+)
 
-
+let david = new Person(
+    "David",
+    ["Sammy"],
+)
+sammy.greet(david);
+sammy.lazyGreet(david);
